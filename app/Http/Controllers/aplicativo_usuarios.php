cb9f084231dd8983a9781dd858e64bd7b86fe892
Black Mamba <mateomena10@gmail.com>
2022-07-31 13:00:19 -0500

    Inical

---
+++ app/Http/Controllers/aplicativo_usuarios.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class aplicativo_usuarios extends Controller
+{
+    //
+}
---
+++ resources/views/includes/panel/menu/doctor.blade.php
@@ -0,0 +1,15 @@
+<li class="nav-item">
+    <a class="nav-link" href="/schedule">
+        <i class="ni ni-calendar-grid-58 text-danger"></i> Gestionar horario
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="/appointments">
+        <i class="ni ni-time-alarm text-primary"></i> Mis Tutorias
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="/patients">
+        <i class="ni ni-single-02 text-info"></i> Mis Estudiantes
+    </a>
+</li>
